cfbaeac052fe6fb757c22eeccf8cde8592e57a53
Avichay3 <aviaimzina312@gmail.com>
2023-06-01 21:46:21 +0300

    update

--- sources/MagicalContainer.hpp
+++ sources/MagicalContainer.hpp
@@ -2,16 +2,31 @@
 
 namespace ariel{
     class MagicalContainer{
+        private:
+            std::vector<int> TheContainer;
+
+            std::vector<int*> PrimeIterPoints;
+
+            std::vector<int*> AscendingIterPoints;
+
+            std::vector<int*> CrossIterPoints;
+
+
+
+        public:
+            MagicalContainer(); //the constructor
+            /*
+            functions for the "Implement necessary methods for adding elements, 
+            removing elements, and retrieving the size of the container".
+            */
+            void addElement(int element)const;
+            void removeElement(int element)const;
+            int size() const;
 
 
 
 
 
 
-
-
-
-
-        
     };
 }
